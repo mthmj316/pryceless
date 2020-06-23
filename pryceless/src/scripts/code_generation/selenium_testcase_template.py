d0791c2e66671bbde0437a91fa52bb78ad3ab39c
mthmj316 <thoma_markus@web.de>
2020-06-23 09:30:50 +0200

    assertNotNull implemented and tested

--- pryceless/src/scripts/code_generation/selenium_testcase_template.py
+++ pryceless/src/scripts/code_generation/selenium_testcase_template.py
@@ -3,17 +3,30 @@ Created on 22.06.2020
 
 @author: mthoma
 '''
-import os
 from string import Template
 
 ASSERT_EQUALS_TEMPLATE = '/home/mthoma/software-dev/code-generator/templates/assert-equals.template'
+ASSERT_NOTNULL_TEMPLATE = '/home/mthoma/software-dev/code-generator/templates/assert-notnull.template'
+
 
 '''
-    Replaces in the given template all occurrences of the
-    variable with the given variable_name by given variable_value.
+    Replaces in the assert-notnull.template template all occurrences of the variables:
+    actual_value replaces actual.
 '''
-def create_assert_equals(expected, actual, attribute):
-    pass
+def create_assert_notnull(actual_value):
+    _template = get_template(ASSERT_NOTNULL_TEMPLATE)
+    return Template(_template).substitute(actual=actual_value)
+
+'''
+    Replaces in the assert-equals.template template all occurrences of the variables:
+    expected_value replaces expected
+    actual_value replaces actual
+    and attribute_name replaces actual.
+'''
+def create_assert_equals(expected_value, actual_value, attribute_name):
+    _template = get_template(ASSERT_EQUALS_TEMPLATE)    
+    return Template(_template).substitute(expected=expected_value, \
+                                          actual=actual_value, attribute=attribute_name)  
 
 '''
     Returns the content of the template with the given name
--- pryceless/src/test_scripts/code_generation/test_selenium_testcase_template.py
+++ pryceless/src/test_scripts/code_generation/test_selenium_testcase_template.py
@@ -6,7 +6,9 @@ Created on 23.06.2020
 import unittest
 
 from scripts.code_generation.selenium_testcase_template import get_template,\
-    ASSERT_EQUALS_TEMPLATE
+    ASSERT_EQUALS_TEMPLATE, create_assert_equals, create_assert_notnull
+    
+WRONG_ASSERT_EQUALS = 'wrong assertEquals'
 
 class Test(unittest.TestCase):
 
@@ -17,14 +19,70 @@ class Test(unittest.TestCase):
 
     def tearDown(self):
         pass
+    
+    '''
+       tests the selenium_testcase_template.create_assert_notnull() function
+       testing replacement by string==space
+    '''
+    def test_create_assert_notnull_space(self):
+        expected = 'assertNotNull(" ");\n'
+        actual = create_assert_notnull('" "')
+        self.assertEqual(expected, actual)
+    
+    '''
+       tests the selenium_testcase_template.create_assert_notnull() function
+       testing replacement by string==<empty string>
+    '''
+    def test_create_assert_notnull_emptystring(self):
+        expected = 'assertNotNull("");\n'
+        actual = create_assert_notnull('""')
+        self.assertEqual(expected, actual)
+        
+    '''
+       tests the selenium_testcase_template.create_assert_notnull() function
+       testing replacement by string==null
+    '''
+    def test_create_assert_notnull_null(self):
+        expected = 'assertNotNull(null);\n'
+        actual = create_assert_notnull('null')
+        self.assertEqual(expected, actual)
+    
+    '''
+        tests the selenium_testcase_template.create_assert_equals() function
+        testing replacement by whitespaces within double quotes
+    '''      
+    def test_create_assert_equals_semikolon_whitespace(self):
+        expected = 'assertEquals( , ,"wrong iam-a-attribute");\n'
+        actual = create_assert_equals(' ', '', 'iam-a-attribute')
+        self.assertEqual(expected, actual, WRONG_ASSERT_EQUALS)
 
+    '''
+        tests the selenium_testcase_template.create_assert_equals() function
+        testing replacement by whitespaces
+    '''   
+    def test_create_assert_equals_whitespace(self):
+        expected = 'assertEquals("", " ","wrong iam-a-attribute");\n'
+        actual = create_assert_equals('""', '" "', 'iam-a-attribute')
+        self.assertEqual(expected, actual, WRONG_ASSERT_EQUALS)
+
+    '''
+        tests the selenium_testcase_template.create_assert_equals() function
+        testing replacement by strings which are not empty
+    '''
+    def test_create_assert_equals(self):
+        expected = 'assertEquals(that is what is expected, that is what is actually is,"wrong iam-a-attribute");\n'
+        actual = create_assert_equals('that is what is expected', \
+                                      'that is what is actually is', \
+                                      'iam-a-attribute')
+        self.assertEqual(expected, actual, WRONG_ASSERT_EQUALS)
 
     '''
         tests the selenium_testcase_template.get_template() function
     '''
     def test_get_template(self):
         template_content = get_template(ASSERT_EQUALS_TEMPLATE)
-        self.assertEqual('assertEquals($expected, $actual,"wrong $attribute");\n', template_content, 'wrong template content')
+        self.assertEqual('assertEquals($expected, $actual,"wrong $attribute");\n', \
+                         template_content, 'wrong template content')
 
 
 if __name__ == "__main__":
