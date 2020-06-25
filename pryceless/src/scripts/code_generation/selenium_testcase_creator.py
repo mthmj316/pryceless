2e914a4eb30ae171589584d633e4334490be152b
mthmj316 <thoma_markus@web.de>
2020-06-25 10:29:21 +0200

    correction tag name test

--- pryceless/src/scripts/code_generation/selenium_testcase_creator.py
+++ pryceless/src/scripts/code_generation/selenium_testcase_creator.py
@@ -30,7 +30,7 @@ def create_unit_test_tag_name(tag_id, expected_tag_name):
     
     test_case_content = ['']
     test_case_content.append(create_tag_under_test_var_assignment(tag_id))
-    test_case_content.append(create_assert_equals('"' + expected_tag_name + '"','tag.getTagName()', 'tag name'))
+    test_case_content.append(create_assert_equals('"%s"' %(expected_tag_name),'tag.getTagName()', 'tag name'))
     
     method_name = convert_tag_id_to_name_in_method(tag_id) + 'TagName'
     
@@ -39,13 +39,12 @@ def create_unit_test_tag_name(tag_id, expected_tag_name):
     return '\n'.join([JAVA_DOC_TEST_TAG_NAM %(tag_id, expected_tag_name), unit_test_method])
 
 '''
-    Converts the given tag_id to the name which is inserted in the testcase method.
+    Converts the given tag_id to the name which is inserted in the test case method.
     link_main_theme --> LinkMainTheme
 '''
 def convert_tag_id_to_name_in_method(tag_id):
     
-    wrk_name = tag_id.replace('_', ' ')
-    
+    wrk_name = tag_id.replace('_', ' ')    
     wrk_name = wrk_name.title()
     
     return wrk_name.replace(' ', '')
