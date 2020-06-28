5519c3eb35f100e96f147a355e6545298fa5e479
mthmj316 <thoma_markus@web.de>
2020-06-28 22:15:20 +0200

    Sibling test case creation

--- pryceless/src/scripts/code_generation/selenium_testcase_creator.py
+++ pryceless/src/scripts/code_generation/selenium_testcase_creator.py
@@ -9,12 +9,67 @@ The following test case can be created by this script:
 
 JAVA_DOC_TEST_TAG_NAME = '/**\n* Test if the tag with id==%s has the tag name==%s.\n*/'
 JAVA_DOC_TEST_PARENT = '/**\n* Test if the tag with id==%s has the parent with the id==%s.\n*/'
-SELENIUM_GET_ATTRIBUTE = '%s.getAttribute("id")'
+SELENIUM_GET_ATTRIBUTE = '%s.getAttribute("%s")'
+SIBLING_CSV_SOURCE_ANNOTATION = '@CsvSource({"%s,preceding-sibling::*[1]", "%s,following-sibling::*[1]"})'
+
+NO_SUCH_ELEMENT_EXCEPTION_CLASS = 'org.openqa.selenium.NoSuchElementException.class'
 
 from scripts.code_generation.selenium_testcase_template import create_tag_under_test_var_assignment,\
     create_unit_test_method, create_assert_equals,\
     create_selenium_webelement_declaration, create_selenium_find_element,\
-    create_selenium_by_id, create_selenium_by_xpath
+    create_selenium_by_xpath, create_assert_throws,\
+    create_parameterized_test_method
+
+'''
+    Creates the test methods for testing the siblings of the html tag with id==tag_id
+    If both predecessor_id and  successor_id is set then one unit test method will be created 
+    tag_id -> html id of the tag for which the siblings are tested
+    predecessor_id  -> id of the tag which must be directly before the tag under test
+                    -> if none then the test will be 
+'''
+def create_unit_test_siblings(tag_id, predecessor_id, successor_id):
+    code = ['']
+    code.append(create_tag_under_test_var_assignment(tag_id))
+    code.append('if(expectedSiblingId == null) {')
+    code.append(__create_unit_test_no_sibling_assertion())
+    code.append('} else {')
+    code.append(__create_unit_test_sibling())
+    code.append('}')
+    
+    variable_dict = {'parameter_sources': __create_unit_test_sibling_csv_source(predecessor_id, successor_id),
+                     'what_is_tested': convert_tag_id_to_name_in_method(tag_id) + 'Sibling',
+                     'parameters': 'final String expectedSiblingId, final String xpath',
+                     'test_method_content': '\n\t'.join(code)}
+    
+    return create_parameterized_test_method(variable_dict)
+
+def __create_unit_test_sibling_csv_source(predecessor_id, successor_id):
+    return SIBLING_CSV_SOURCE_ANNOTATION %(predecessor_id, successor_id)
+
+'''
+    Creates the test for when a sibling is expected.
+    example:
+        final WebElement sibling = tag.findElement(By.xpath(xpath));
+        assertEquals(expectedSiblingId, sibling.getAttribute("id"), "wrong sibling");
+        
+'''
+def __create_unit_test_sibling():
+    code = ['']
+    code.append(create_selenium_webelement_declaration('sibling', create_selenium_find_element('tag', create_selenium_by_xpath('xpath'))))
+    code.append(create_assert_equals('expectedSiblingId', SELENIUM_GET_ATTRIBUTE %('sibling', 'id'), 'sibling'))
+    
+    return '\n\t'.join(code) 
+
+'''
+    Creates the asserThrows expression for the case when no sibling is expected
+    examle:
+        assertThrows(NoSuchElementException.class, () -> tag.findElement(By.xpath(xpath)));
+'''
+def __create_unit_test_no_sibling_assertion():
+    by_xpath = create_selenium_by_xpath('xpath')
+    find_by = create_selenium_find_element('tag', by_xpath)
+    
+    return '\t' + create_assert_throws(NO_SUCH_ELEMENT_EXCEPTION_CLASS, find_by)
 
 '''
     Creates the unit test method for testing the parent of an html tag.
@@ -39,7 +94,7 @@ def create_unit_test_parent(tag_id, parent_tag_id):
     parent_tag_var = create_selenium_webelement_declaration('parent',parent_find_expr)
     
     test_case_content.append(parent_tag_var)
-    test_case_content.append(create_assert_equals('"%s"' %parent_tag_id, SELENIUM_GET_ATTRIBUTE %'parent', 'parent'))
+    test_case_content.append(create_assert_equals('"%s"' %parent_tag_id, SELENIUM_GET_ATTRIBUTE %('parent', 'id'), 'parent'))
     
     method_name = convert_tag_id_to_name_in_method(tag_id) + 'Parent'
     
--- pryceless/src/test_scripts/code_generation/test_selenium_testcase_creator.py
+++ pryceless/src/test_scripts/code_generation/test_selenium_testcase_creator.py
@@ -5,10 +5,28 @@ Created on 25.06.2020
 '''
 import unittest
 from scripts.code_generation.selenium_testcase_creator import convert_tag_id_to_name_in_method,\
-    create_unit_test_tag_name, create_unit_test_parent
+    create_unit_test_tag_name, create_unit_test_parent,\
+    create_unit_test_siblings
 
+SIBLING_EXPECTED_RESULT = ('@ParameterizedTest\n'
+    '@CsvSource({"login_error_message,preceding-sibling::*[1]", "login_name,follwoing-sibling::*[1]"})\n'
+    'public void testLoginPasswordSibling(final String expectedSiblingId, final String xpath){\n'
+    '\tfinal WebElement tag = DRIVER.findElement(By.id("login_password"));\n'
+    '\tif(expectedSiblingId == null) {\n'
+    '\t\tassertThrows(org.openqa.selenium.NoSuchElementException.class, () -> tag.findElement(By.xpath(xpath)));\n'     
+    '\t} else {\n'
+    '\t\tfinal WebElement sibling = tag.findElement(By.xpath(xpath));\n'
+    '\t\tassertEquals(expectedSiblingId, sibling.getAttribute("id"), "wrong sibling");\n'
+    '\t}\n'
+    '}'
+    )
 
 class Test(unittest.TestCase):
+    
+    def test_create_unit_test_siblings(self):
+        actual = create_unit_test_siblings('login_password', 'login_error_message', 'login_name')
+        self.maxDiff = None
+        self.assertEqual(SIBLING_EXPECTED_RESULT, actual)
     
     def test_create_unit_test_parent(self):
         expected = ('/**\n* Test if the tag with id==main_header has the parent with the id==main_container.\n*/'
@@ -21,7 +39,6 @@ class Test(unittest.TestCase):
                     '\n}'
                     )
         actual = create_unit_test_parent('main_header', 'main_container')
-        self.maxDiff = None
         self.assertEqual(expected, actual)
     
     def test_create_unit_test_tag_name(self):
@@ -51,7 +68,6 @@ class Test(unittest.TestCase):
         actual = convert_tag_id_to_name_in_method('html_id')
         self.assertEqual(expected, actual)
 
-
     def test_convert_tag_id_to_name_in_method_no_underscore(self):
         expected = 'Html'
         actual = convert_tag_id_to_name_in_method('html')
@@ -61,7 +77,6 @@ class Test(unittest.TestCase):
         expected = 'H'
         actual = convert_tag_id_to_name_in_method('h')
         self.assertEqual(expected, actual)
-
 
 if __name__ == "__main__":
     unittest.main()
