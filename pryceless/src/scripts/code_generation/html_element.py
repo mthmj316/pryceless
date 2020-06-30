71a94530cfba9af4b794fdce041fd20fd35df52b
mthmj316 <thoma_markus@web.de>
2020-06-30 22:04:50 +0200

    html classes finished

--- pryceless/src/scripts/code_generation/html_element.py
+++ pryceless/src/scripts/code_generation/html_element.py
@@ -3,6 +3,7 @@ Created on 30.06.2020
 
 @author: mthoma
 '''
+from orca.scripts import self_voicing
 
 class HTMLElement(object):
     '''
@@ -14,16 +15,92 @@ class HTMLElement(object):
         '''
         Constructor
         '''
-        self.element_name = element_name
-        self.element_id = element_id
-        self.parent_id
-        self.predecessor_id
-        self.successor_id
-        self.css_directory = {}
-        self.attribute_directory = {}
+        self._element_name = element_name
+        self._element_id = element_id
+        self._parent_id
+        self._predecessor_id
+        self._successor_id
+        self._css_rule = {}
+        self._attribute = {}
+    
+    @property
+    def element_id(self):
+        self._element_id
         
-    def add_css_property(self, property_name, property_value):
-        self.css_directory[property_name] = property_value
+    @element_id.setter
+    def element_id(self, value):
+        self._element_id = value
+    
+    @element_id.deleter
+    def element_id(self):
+        del self._element_id
+        
+    @property
+    def element_name(self):
+        self._element_name
+        
+    @element_name.setter
+    def element_name(self, value):
+        self._element_name = value
+    
+    @element_name.deleter
+    def element_name(self):
+        del self._element_name
+        
+    @property
+    def parent_id(self):
+        self._parent_id
+        
+    @parent_id.setter
+    def parent_id(self, value):
+        self._parent_id = value
+    
+    @parent_id.deleter
+    def parent_id(self):
+        del self._parent_id
+        
+    @property
+    def predecessor_id(self):
+        self._predecessor_id
+        
+    @predecessor_id.setter
+    def predecessor_id(self, value):
+        self._predecessor_id = value
+    
+    @predecessor_id.deleter
+    def predecessor_id(self):
+        del self._predecessor_id
+
+    @property
+    def successor_id(self):
+        self._successor_id
+        
+    @successor_id.setter
+    def successor_id(self, value):
+        self._successor_id = value
+    
+    @successor_id.deleter
+    def successor_id(self):
+        del self._successor_id
+    
+    @property
+    def css_rule(self):
+        self._css_rule
+        
+    @css_rule.setter
+    def css_rule(self, value):
+        self._css_rule = value
+    
+    def add_css_rule(self, property_name, property_value):
+        self._css_rule[property_name] = property_value
+    
+    @property
+    def attribute(self):
+        self._attribute
+        
+    @attribute.setter
+    def attribute(self, value):
+        self._attribute = value
     
     def add_attribute(self, attribute_name, attribute_value):
-        self.attribute_directory[attribute_name] = attribute_value    
+        self._attribute[attribute_name] = attribute_value    
--- pryceless/src/scripts/code_generation/html_page.py
+++ pryceless/src/scripts/code_generation/html_page.py
@@ -13,9 +13,41 @@ class HTMLPage(object):
         '''
         Constructor
         '''
-        self.page_name = page_name
-        self.url = url
-        self.html_elements = []
+        self._page_name = page_name
+        self._url = url
+        self._html_elements = []    
+    
+    @property
+    def page_name(self):
+        self._page_name
+        
+    @page_name.setter
+    def page_name(self, value):
+        self._page_name = value
+        
+    @page_name.deleter
+    def page_name(self):
+        del self._page_name
+        
+    @property
+    def url(self):
+        self._url
+        
+    @url.setter
+    def url(self, value):
+        self._url = value
+        
+    @url.deleter
+    def url(self):
+        del self._url
+    
+    @property
+    def html_elements(self):
+        self._html_elements
+        
+    @html_element.setter
+    def html_element(self, value):
+        self._html_elements = value
         
     def add_html_elements(self, html_element):
         self.html_elements.append(html_element)
