280de625821b398d5469280bd893a72b3c751164
mthmj316 <thoma_markus@web.de>
2020-07-03 07:14:10 +0200

    setup.py and __init__ files

---
+++ pryceless/src/scripts/selenium_test_generator/__init__.py
@@ -0,0 +1,3 @@
+# __all__ = ['generator_constants', 'html_element',
+#           'html_page', 'selenium_testcase_creator',
+#           'selenium_testcase_template', 'selenium_testclass_creator']
---
+++ pryceless/src/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+setup(
+    name='pryceless',
+    version="0.1",
+    packages=find_packages(),
+)
