ace5e92d933d1d064efc106380a3ed70d778aef9
mthmj316 <thoma_markus@web.de>
2020-07-03 14:04:20 +0200

    setup.py adjustments

--- pryceless/setup.py
+++ pryceless/setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
+
 setup(
     name='pryceless',
     version="0.1",
-    packages=find_packages(),
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    package_data={
+        '': ['*.template']
+    }
 )
