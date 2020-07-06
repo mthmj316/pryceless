c4d9548173c7dbfa4395ff9437b21285abe3fac5
mthmj316 <thoma_markus@web.de>
2020-07-06 11:49:42 +0200

    setup wrong conf

--- pryceless/setup.py
+++ pryceless/setup.py
@@ -6,9 +6,6 @@ setup(
     version="0.1",
     package_dir={'': 'src'},
     packages=find_packages('src'),
-    package_data={
-        '': ['*.template']
-    },
     package_data={'templates': ['*.template']},
     include_package_data=True,
 )
