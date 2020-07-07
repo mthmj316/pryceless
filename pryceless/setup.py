281ff57cf4a456a302690e25d21d663a98c9013a
mthmj316 <thoma_markus@web.de>
2020-07-07 09:21:16 +0200

    setup.py

--- pryceless/setup.py
+++ pryceless/setup.py
@@ -5,7 +5,7 @@ setup(
     name='pryceless',
     version="0.1",
     package_dir={'': 'src'},
-    packages=['scripts', 'templates'],
+    packages=['scripts', 'templates', 'tests'],
     package_data={'': ['*.template', '*.expected']},
     include_package_data=True,
 )
