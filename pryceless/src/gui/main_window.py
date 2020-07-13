5ccc3768552344e28a3c08ba165802981d327135
mthmj316 <thoma_markus@web.de>
2020-07-13 09:16:33 +0200

    main window created

---
+++ pryceless/src/gui/main_window.py
@@ -0,0 +1,20 @@
+'''
+Created on 13.07.2020
+
+@author: mthoma
+'''
+import tkinter as tk
+
+class MainWindow(tk.Frame):
+    '''
+    classdocs
+    '''
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainWindow(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
