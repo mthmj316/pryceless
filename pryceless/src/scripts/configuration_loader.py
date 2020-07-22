dee75f10d86063b1d9b7b54aa9802211cb2df7af
mthmj316 <thoma_markus@web.de>
2020-07-22 21:13:36 +0200

    configuration loader implement / test

--- pryceless/src/scripts/configuration_loader.py
+++ pryceless/src/scripts/configuration_loader.py
@@ -6,6 +6,7 @@ Created on 20.07.2020
 import os
 import io
 import collections as coll
+from attr import attr
 
 HTML_TAG_CONF = None
 
@@ -44,140 +45,210 @@ def load_conf_files(conf_file_name):
 def load_html_tag(tag_name):
     '''
     '''
+    tag_conf = load_from_html_conf(tag_name)
+    tag_conf_split = tag_conf.split(';')
+    
+    attr_conf = tag_conf_split[0]
+    event_conf =  tag_conf_split[1]
+    parent_conf =  tag_conf_split[2]
+    children_conf =  tag_conf_split[3]
+    has_end_tag_conf =  tag_conf_split[4]
+        
+    result = {
+            'tag': tag_name,
+            'events': __load_html_tag_events(event_conf),
+            'attributes': __load_html_tag_attr(attr_conf),
+            'parents': __load_html_tag_parents(parent_conf),
+            'children': __load_html_tag_children(children_conf),
+            'has_end_tag': has_end_tag_conf == 'true'
+        }
+    
+    return result
+
+def __load_html_tag_children(children_conf):
     pass
 
+def __load_html_tag_parents(parent_conf):
+    pass
+
+def __load_html_tag_events(event_conf):
+    pass
+    
+def __load_html_tag_attr(attr_conf):
+    
+    attributes = []
+    
+    if len(attr_conf) > 0:
+        attr_conf_split = attr_conf.split(',')
+        if attr_conf_split[0] == '[GLOBAL_ATTRIBUTES]':
+            attributes.append(load_global_attributes())
+        
+        if len(attr_conf_split) > 1:
+            idx = 1
+            while idx < len(attr_conf_split):
+                attributes.append(attr_conf_split[idx])
+                idx += 1
+    return attributes
+    
 def load_global_attributes():
     '''
     Loads from the html_tag.conf the [GLOBAL_ATTRIBUTES] attributes
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('GLOBAL_ATTRIBUTES')
+    return attr_as_string.split(',')
 
-def load_heading_contents():
+def load_heading_content():
     '''
     Loads from the html_tag.conf the [HEADING_CONTENT] tags
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('HEADING_CONTENT')
+    return attr_as_string.split(',')
  
 def load_phrasing_content():
     '''
     Loads from the html_tag.conf the [PHRASING_CONTENT] tags
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('PHRASING_CONTENT')
+    return attr_as_string.split(',')
 
 def load_flow_content():
     '''
     Loads from the html_tag.conf the [FLOW_CONTENT] tags
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('FLOW_CONTENT')
+    return attr_as_string.split(',')
 
 def load_metadata_content():
     '''
     Loads from the html_tag.conf the [METADATA_CONTENT] tags
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('METADATA_CONTENT')
+    return attr_as_string.split(',')
 
 def load_embedded_content():
     '''
     Loads from the html_tag.conf the [EMBEDDED_CONTENT] tags
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('EMBEDDED_CONTENT')
+    return attr_as_string.split(',')
 
 def load_interactive_content():
     '''
     Loads from the html_tag.conf the [INTERACTIVE_CONTENT] tags
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('INTERACTIVE_CONTENT')
+    return attr_as_string.split(',')
 
 def load_palpable_content():
     '''
     Loads from the html_tag.conf the [PALPABLE_CONTENT] tags
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('PALPABLE_CONTENT')
+    return attr_as_string.split(',')
 
 def load_sectioning_content():
     '''
     Loads from the html_tag.conf the [SECTIONING_CONTENT] tags
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('SECTIONING_CONTENT')
+    return attr_as_string.split(',')
 
 def load_script_supporting_elements():
     '''
     Loads from the html_tag.conf the [SCRIPT_SUPPORTING_ELEMENTS] tags
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('SCRIPT_SUPPORTING_ELEMENTS')
+    return attr_as_string.split(',')
 
 def load_all_events():
     '''
     Loads from the html_tag.conf all events
     and returns it as array.
     '''
-    pass
+    all_events = []
+    all_events.append(load_window_events())
+    all_events.append(load_form_events())
+    all_events.append(load_keyboard_events())
+    all_events.append(load_mouse_events())
+    all_events.append(load_misc_events())
+    all_events.append(load_media_events())
+    all_events.append(load_clipboard_events())
+    all_events.append(load_drag_events())
+    return all_events
 
 def load_window_events():
     '''
     Loads from the html_tag.conf the [WINDOW_EVENTS] events
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('WINDOW_EVENTS')
+    return attr_as_string.split(',')
 
 def load_form_events():
     '''
     Loads from the html_tag.conf the [FORM_EVENTS] events
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('FORM_EVENTS')
+    return attr_as_string.split(',')
 
 def load_keyboard_events():
     '''
     Loads from the html_tag.conf the [KEYBOARD_EVENTS] events
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('KEYBOARD_EVENTS')
+    return attr_as_string.split(',')
 
 def load_mouse_events():
     '''
     Loads from the html_tag.conf the [MOUSE_EVENTS] events
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('MOUSE_EVENTS')
+    return attr_as_string.split(',')
 
 def load_drag_events():
     '''
     Loads from the html_tag.conf the [DRAG_EVENTS] events
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('DRAG_EVENTS')
+    return attr_as_string.split(',')
 
 def load_clipboard_events():
     '''
     Loads from the html_tag.conf the [CLIPBOARD_EVENTS] events
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('CLIPBOARD_EVENTS')
+    return attr_as_string.split(',')
 
 def load_media_events():
     '''
     Loads from the html_tag.conf the [MEDIA_EVENTS] events
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('MEDIA_EVENTS')
+    return attr_as_string.split(',')
 
 def load_misc_events():
     '''
     Loads from the html_tag.conf the [MISC_EVENTS] events
     and returns it as array.
     '''
-    pass
+    attr_as_string = load_from_html_conf('MISC_EVENTS')
+    return attr_as_string.split(',')
 
 def load_html_description():
     '''
--- pryceless/src/tests/test_configuration_loader.py
+++ pryceless/src/tests/test_configuration_loader.py
@@ -9,8 +9,230 @@ from scripts import configuration_loader
 H1_EXPECTED_RESULT = '[GLOBAL_ATTRIBUTES];[ALL_EVENTS];[HEADING_CONTENT];[PHRASING_CONTENT],[HEADING_CONTENT];true'
 GLOBAL_ATTRIBUTES_EXPECTED_RESULT = 'accesskey,class,contenteditable,dir,draggable,hidden,id,lang,spellcheck,style,tabindex,title,translate'
 P_EXPECTED_RESULT = '[GLOBAL_ATTRIBUTES];;[PHRASING_CONTENT];[FLOW_CONTENT];true'
+GLOBAL_ATTRIBUTES = ['accesskey','class','contenteditable','dir','draggable','hidden','id','lang','spellcheck','style','tabindex','title','translate']
+WINDOW_EVENTS = ['onafterprint','onbeforeprint','onbeforeunload','onerror','onhashchange','onload','onmessage','onoffline','ononline','onpagehide','onpageshow','onpopstate','onresize','onstorage','onunload']
+FORM_EVENTS = ['onblur','onchange','oncontextmenu','onfocus','oninput','oninvalid','onreset','onsearch','onselect','onsubmit']
+KEYBOARD_EVENTS = ['onkeydown','onkeypress','onkeyup']
+MOUSE_EVENTS = ['onclick','ondblclick','onmousedown','onmousemove','onmouseout','onmouseover','onmouseup','onmousewheel','onwheel']
+DRAG_EVENTS = ['ondrag','ondragend','ondragenter','ondragleave','ondragover','ondragstart','ondrop','onscroll']
+CLIPBOARD_EVENTS = ['oncopy','oncut','onpaste']
+MEDIA_EVENTS = ['onabort','oncanplay','oncanplaythrough','oncuechange','ondurationchange','onemptied','onended','onerror','onloadeddata','onloadedmetadata','onloadstart','onpause','onplay','onplaying','onprogress','onratechange','onseeked','onseeking','onstalled','onsuspend','ontimeupdate','onvolumechange','onwaiting']
+MISC_EVENTS = ['ontoggle']
+FLOW_CONTENT = ['a','abbr','address','area','article','aside','audio','b','bdi','bdo','blockquote','br','button','canvas','cite','code','data','datalist','del','details','dfn','dialog','div','dl','em','embed','fieldset','figure','footer','form','h1','h2','h3','h4','h5','h6','header','hgroup','hr','i','iframe','img','input','ins','kbd','label','link','main','map','mark','MathML math','menu','meta','meter','nav','noscript','object','ol','output','p','picture','pre','progress','q','ruby','s','samp','script','section','select','slot','small','span','strong','sub','sup','SVG svg','table','template','textarea','time','u','ul','var','video','wbr','autonomous custom elements','text']
+PHRASING_CONTENT = ['a','abbr','area','audio','b','bdi','bdo','br','button','canvas','cite','code','data','datalist','del','dfn','em','embed','i','iframe','img','input','ins','kbd','label','link','map','mark','MathML math','meta','meter','noscript','object','output','picture','progress','q','ruby','s','samp','script','select','slot','small','span','strong','sub','sup','SVG svg','template','textarea','time','u','var','video','wbr','autonomous custom elements','text']
+HEADING_CONTENT = ['h1','h2','h3','h4','h5','h6','hgroup']
+METADATA_CONTENT = ['base','link','meta','noscript','script','style','template','title']
+EMBEDDED_CONTENT = ['audio','canvas','embed','iframe','img','MathML math','object','picture','SVG svg','video']
+INTERACTIVE_CONTENT = ['a','audio','button','details','embed','iframe','img','input','label','object','select','textarea','video']
+PALPABLE_CONTENT = ['a','abbr','address','article','aside','audio','b','bdi','bdo','blockquote','button','canvas','cite','code','data','details','dfn','div','dl','em','embed','fieldset','figure','footer','form','h1','h2','h3','h4','h5','h6','header','hgroup','i','iframe','img','input','ins','kbd','label','main','map','mark','MathML math','menu','meter','nav','object','ol','output','p','pre','progress','q','ruby','s','samp','section','select','small','span','strong','sub','sup','SVG svg','table','textarea','time','u','ul','var','video','autonomous custom elements','text']
+SECTIONING_CONTENT = ['article','aside','nav','section']
+SCRIPT_SUPPORTING_ELEMENTS = ['script','template']
 
 class Test(unittest.TestCase):
+    
+    def test_load_html_tag_h6(self):
+        '''
+        [h6]::[GLOBAL_ATTRIBUTES];[ALL_EVENTS];[HEADING_CONTENT];[PHRASING_CONTENT],[HEADING_CONTENT];true
+        '''
+        
+        parents = []
+        parents.append(PHRASING_CONTENT)
+        parents.append(HEADING_CONTENT)
+        
+        all_events = []
+        all_events.append(WINDOW_EVENTS)
+        all_events.append(FORM_EVENTS)
+        all_events.append(KEYBOARD_EVENTS)
+        all_events.append(MOUSE_EVENTS)        
+        all_events.append(DRAG_EVENTS)        
+        all_events.append(CLIPBOARD_EVENTS)        
+        all_events.append(MEDIA_EVENTS)        
+        all_events.append(MISC_EVENTS)
+        
+        expected = {
+            'tag': 'h6',
+            'events': all_events,
+            'attributes': GLOBAL_ATTRIBUTES,
+            'parents': parents,
+            'children': HEADING_CONTENT,
+            'has_end_tag': False
+            }
+        
+        self.maxDiff = None
+        actual = configuration_loader.load_html_tag('h6')
+        self.assertDictEqual(expected, actual)
+    
+    def test_load_html_tag_base(self):
+        '''
+        [base]::[GLOBAL_ATTRIBUTES],href,target;;;head;false
+        '''
+        attr_list = ['href','target']
+        attr_list.append(GLOBAL_ATTRIBUTES)
+        
+        expected = {
+            'tag': 'base',
+            'events': [],
+            'attributes': attr_list,
+            'parents': ['head'],
+            'children': [],
+            'has_end_tag': False
+            }
+        
+        self.maxDiff = None
+        actual = configuration_loader.load_html_tag('base')
+        self.assertDictEqual(expected, actual)
+    
+    def test_load_all_events(self):
+        '''
+        Test: Loading from the html_tag.conf all events
+        and returns it as array.
+        '''
+        all_events = []
+        all_events.append(WINDOW_EVENTS)
+        all_events.append(FORM_EVENTS)
+        all_events.append(KEYBOARD_EVENTS)
+        all_events.append(MOUSE_EVENTS)        
+        all_events.append(DRAG_EVENTS)        
+        all_events.append(CLIPBOARD_EVENTS)        
+        all_events.append(MEDIA_EVENTS)        
+        all_events.append(MISC_EVENTS)
+        
+        actual = configuration_loader.load_all_events()
+        self.assertCountEqual(all_events, actual)
+    
+    def test_load_window_events(self):
+        '''
+        Test: Loading from the html_tag.conf the [WINDOW_EVENTS] events
+        and returns it as array.
+        '''
+        actual = configuration_loader.load_window_events()
+        self.assertCountEqual(WINDOW_EVENTS, actual)
+    
+    def test_load_form_events(self):
+        '''
+        Test: Loading from the html_tag.conf the [FORM_EVENTS] events
+        and returns it as array.
+        '''
+        actual = configuration_loader.load_form_events()
+        self.assertCountEqual(FORM_EVENTS, actual)
+    
+    def test_load_keyboard_events(self):
+        '''
+        Test: Loading from the html_tag.conf the [KEYBOARD_EVENTS] events
+        and returns it as array.
+        '''
+        actual = configuration_loader.load_form_events()
+        self.assertCountEqual(FORM_EVENTS, actual)
+    
+    def test_load_mouse_events(self):
+        '''
+        Test: Loading from the html_tag.conf the [MOUSE_EVENTS] events
+        and returns it as array.
+        '''
+        actual = configuration_loader.load_form_events()
+        self.assertCountEqual(FORM_EVENTS, actual)
+    
+    def load_drag_events(self):
+        '''
+        Test: Loading from the html_tag.conf the [DRAG_EVENTS] events
+        and returns it as array.
+        '''
+        actual = configuration_loader.load_drag_events()
+        self.assertCountEqual(DRAG_EVENTS, actual)
+    
+    def test_load_clipboard_events(self):
+        '''
+        Test: Loading from the html_tag.conf the [CLIPBOARD_EVENTS] events
+        and returns it as array.
+        '''
+        actual = configuration_loader.load_clipboard_events()
+        self.assertCountEqual(CLIPBOARD_EVENTS, actual)
+    
+    def test_load_media_events(self):
+        '''
+        Test: Loading from the html_tag.conf the [MEDIA_EVENTS] events
+        and returns it as array.
+        '''
+        actual = configuration_loader.load_media_events()
+        self.assertCountEqual(MEDIA_EVENTS, actual)
+    
+    def test_load_misc_events(self):
+        '''
+        Test: Loading from the html_tag.conf the [MISC_EVENTS] events
+        and returns it as array.
+        '''
+        actual = configuration_loader.load_misc_events()
+        self.assertCountEqual(MISC_EVENTS, actual)
+    
+    def test_load_script_supporting_elements(self):
+        '''
+        Test the onfiguration_loader.load_script_supporting_elements() function
+        '''
+        actual = configuration_loader.load_script_supporting_elements()
+        self.assertCountEqual(SCRIPT_SUPPORTING_ELEMENTS, actual)
+    
+    def test_load_sectioning_content(self):
+        '''
+        Test the onfiguration_loader.load_sectioning_content() function
+        '''
+        actual = configuration_loader.load_sectioning_content()
+        self.assertCountEqual(SECTIONING_CONTENT, actual)
+    
+    def test_load_palpable_content(self):
+        '''
+        Test the onfiguration_loader.load_palpable_content() function
+        '''
+        actual = configuration_loader.load_palpable_content()
+        self.assertCountEqual(PALPABLE_CONTENT, actual)
+    
+    def test_load_interactive_content(self):
+        '''
+        Test the onfiguration_loader.load_interactive_content() function
+        '''
+        actual = configuration_loader.load_interactive_content()
+        self.assertCountEqual(INTERACTIVE_CONTENT, actual)
+    
+    def test_load_embedded_content(self):
+        '''
+        Test the onfiguration_loader.load_embedded_content() function
+        '''
+        actual = configuration_loader.load_embedded_content()
+        self.assertCountEqual(EMBEDDED_CONTENT, actual)
+    
+    def test_load_metadata_content(self):
+        '''
+        Test the onfiguration_loader.load_metadata_content() function
+        '''
+        actual = configuration_loader.load_metadata_content()
+        self.assertCountEqual(METADATA_CONTENT, actual)
+    
+    def test_load_flow_content(self):
+        '''
+        Test the onfiguration_loader.load_flow_content() function
+        '''
+        actual = configuration_loader.load_flow_content()
+        self.assertCountEqual(FLOW_CONTENT, actual)
+    
+    def test_load_phrasing_content(self):
+        '''
+        Test the onfiguration_loader.load_phrasing_content() function
+        '''
+        actual = configuration_loader.load_phrasing_content()
+        self.assertCountEqual(PHRASING_CONTENT, actual)
+    
+    def test_load_heading_content(self):
+        '''
+        Test the onfiguration_loader.load_heading_contents() function
+        '''
+        actual = configuration_loader.load_heading_content()
+        self.assertCountEqual(HEADING_CONTENT, actual)
+    
+    def test_load_global_attributes(self):
+        '''
+        Test the onfiguration_loader.load_global_attributes() function
+        '''
+        actual = configuration_loader.load_global_attributes()
+        self.assertCountEqual(GLOBAL_ATTRIBUTES, actual)
     
     def test_html_tag_conf_setting(self):
         '''
