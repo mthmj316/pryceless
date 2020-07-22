2b8378613de2bfb6fe6fbbfafe0d58c611aba347
mthmj316 <thoma_markus@web.de>
2020-07-22 08:36:37 +0200

    thml_tag created

---
+++ pryceless/src/scripts/html_tag.py
@@ -0,0 +1,17 @@
+'''
+Created on 22.07.2020
+
+@author: mthoma
+'''
+
+class HtmlTag(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
