c0ab176a78c0ccdead823946bc812560e425d869
mthmj316 <thoma_markus@web.de>
2020-09-07 21:34:03 +0200

    version change form v0.2.1 to v0.3

--- pryceless/src/main/application.py
+++ pryceless/src/main/application.py
@@ -5,7 +5,13 @@ Created on 04.08.2020
 '''
 from main_window.main_window_cntlr import MainWindowCNTLR
 
+VERSION = 'v0.3'
+
 if __name__ == '__main__':
     
+    title = ' '.join(['Pryceless', VERSION])
+    
+    print('application    starting ' + title)
+    
     main_window = MainWindowCNTLR()
-    main_window.show('Pryceless v0.2.1')
+    main_window.show(title)
