e2e92f93fb52e2d139864bce20d947f29e9efeb0
mthmj316 <thoma_markus@web.de>
2020-09-24 09:19:45 +0200

    year added to the window title

--- pryceless/src/main/application.py
+++ pryceless/src/main/application.py
@@ -4,12 +4,13 @@ Created on 04.08.2020
 @author: mthoma
 '''
 from main_window.main_window_cntlr import MainWindowCNTLR
+from datetime import datetime
 
-VERSION = 'v0.3'
+VERSION = ' v0.3'
 
 if __name__ == '__main__':
     
-    title = ' '.join(['Pryceless', VERSION])
+    title = ''.join(['Pryceless', VERSION, ' (2020-',str(datetime.now().year), ')'])
     
     print('application    starting ' + title)
     
