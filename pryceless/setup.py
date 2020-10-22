6f10c2d15f02cea7c28ce4fd6325e369d37f2280
mthmj316 <thoma_markus@web.de>
2020-10-22 21:45:59 +0200

    Cleaning up sources

--- pryceless/setup.py
+++ pryceless/setup.py
@@ -8,10 +8,7 @@ setup(
     name='pryceless',
     version="0.1",
     package_dir={'': 'src'},
-    packages=['scripts', 'templates', 'tests', 'gui',
-              'conf', 'utils', 'controller', 'main', 
-              'model', 'interfaces', 'main_window', 'controls',
-              'dialogs'],
+    packages=['conf','controls','dialogs','main','main_window','scripts','templates','tests','utils'],
     package_data={'': ['*.template', '*.expected', '*.conf']},
     include_package_data=True,
 )
